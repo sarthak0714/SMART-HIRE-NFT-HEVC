3f82eb7a5da475e7881d4619f0c946caf5df591e
Sarthak0714 <91636065+sarthak0714@users.noreply.github.com>
2023-03-21 22:34:24 +0530

    Add files via upload

---
+++ badge-dataentry/contracts/badge2.sol
@@ -0,0 +1,32 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+contract badge2 is ERC721 {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721 ("Completion Badge", "Excel Data Entry Badge") {
+    console.log("This is a NFT contract.");
+  }
+
+  function makeBadge2() public{
+    uint256 newItemId= _tokenIds.current();
+    _safeMint(msg.sender,newItemId);
+    tokenURI(newItemId);
+    _tokenIds.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns(string memory){
+    require(_exists(_tokenId));
+    console.log("An NFT w/ ID %s has been minted to %s", _tokenId, msg.sender);
+    return "https://raw.githubusercontent.com/sarthak0714/METADATA/main/wb.json";
+  }
+
+}
